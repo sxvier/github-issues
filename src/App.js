4c5a37c1980d7d752a4e699fd89d07a0ab0c680f
SemaJ Solomon <sxviersolomon@gmail.com>
2021-08-23 10:51:02 -0400

    restarted from initial commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
 import './App.css';
-import IssueList from './components/IssueList';
+
 
 function App() {
   return (
     <div className="App">
-      <IssueList />
+      
     </div>
   );
 }
